8696234a1c174f5f869ef55f2b4dbc7c8778f424
Ji-Woon Hong <jackson@Ji-Woonui-MacBookPro.local>
2023-03-13 07:36:32 +0900

    til 17. React Data Fetching

---
+++ job-board/server/resolver.js
@@ -0,0 +1,11 @@
+import {Company, Job} from "./db.js";
+
+export const resolvers = {
+    Query: {
+            jobs: () => Job.findAll()
+    },
+
+    Job: {
+        company: (job) => Company.findById(job.companyId)
+    }
+}
