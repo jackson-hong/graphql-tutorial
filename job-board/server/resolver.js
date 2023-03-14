363e1ea4b9c8f5c6c9bd38bd1c7d19712586fb4a
jackson-hong <jackson@Ji-Woonui-MacBookPro.local>
2023-03-15 06:22:35 +0900

    git account test

--- job-board/server/resolver.js
+++ job-board/server/resolver.js
@@ -2,10 +2,17 @@ import {Company, Job} from "./db.js";
 
 export const resolvers = {
     Query: {
-            jobs: () => Job.findAll()
+            job: (root, { id }) => Job.findById(id)
+            ,
+            jobs: () => Job.findAll(),
+            company: (root, { id }) => Company.findById(id)
     },
 
     Job: {
         company: (job) => Company.findById(job.companyId)
+    },
+
+    Company: {
+        jobs: (company) => Job.findAll((job) => job.companyId === company.id)
     }
 }
